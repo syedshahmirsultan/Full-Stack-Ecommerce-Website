058169fa7f1a710a33def24ce92612306b45c3dc
syedshahmirsultan <syedshahmirsultan@gmail.com>
2024-06-03 18:39:01 +0500

    Updated the code

--- types.ts
+++ types.ts
@@ -1,40 +1,39 @@
-import { Image } from "sanity"
-export interface singleProductTypeImage{
-  _type: string,
-  _key: string,
+import { Image } from "sanity";
+
+export interface singleProductTypeImage {
+  _type: string;
+  _key: string;
   asset: {
-    _ref: string,
-    _type: string
-  }
-}
-export interface singleProductTypeDescription{
-    _key: string,
-    markDefs: [],
-    children: any[],
-    _type: string,
-    style: string
+    _ref: string;
+    _type: string;
+  };
 }
 
+export interface singleProductTypeDescription {
+  _key: string;
+  markDefs: [];
+  children: any[];
+  _type: string;
+  style: string;
+}
 
-export interface singleProductTypeSlug { 
-  current: string,
-   _type: string
-   }
+export interface singleProductTypeSlug {
+  current: string;
+  _type: string;
+}
 
 export default interface singleProductType {
-        quantity: number,
-        producttype:string,
-        image: Image[],
-        productname: string,
-        slug: string[],
-        price: number,
-        size: string[],
-        description:singleProductTypeDescription[]
-      }
-
-
-export  interface allProductsFetcherType {
-    result :Array<singleProductType>,
-    query:string
+  quantity: number;
+  producttype: string;
+  image: Image[];
+  productname: string;
+  slug: singleProductTypeSlug;  // Changed from string[] to singleProductTypeSlug
+  price: number;
+  size: string[];
+  description: singleProductTypeDescription[];
 }
 
+export interface allProductsFetcherType {
+  result: Array<singleProductType>;
+  query: string;
+}
