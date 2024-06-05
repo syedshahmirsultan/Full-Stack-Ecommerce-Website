4309ab88e488dc1e81de66357bcab791d0db9244
syedshahmirsultan <syedshahmirsultan@gmail.com>
2024-06-05 22:13:14 +0500

    updated search folder

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,19 +1,53 @@
+// "use client"
+// import React, { useEffect, useState } from 'react';
+// import { useSearchParams } from 'next/navigation';
+// import { useRouter } from 'next/router';
+// import AllProductGridViewer from '../components/AllProductGridViewer';
+// import { searchProductsFetcherFromSanity } from '@/utils/apiCalling';
+// import { allProductsFetcherType } from '../../../types';
+
+// const Search = () => {
+//     const router = useRouter();
+//     const params = useSearchParams();
+//     const [productData, setProductData] = useState<allProductsFetcherType | null>(null);
+    
+//     useEffect(() => {
+//         if (!params.has("query")) {
+//             router.push('/products');
+//         } else {
+//             const searchValue = params.get("query") as string;
+//             const fetchData = async () => {
+//                 const data = await searchProductsFetcherFromSanity(searchValue);
+//                 setProductData(data as allProductsFetcherType);
+//             };
+//             fetchData();
+//         }
+//     }, [params, router]);
+    
+//     if (!productData) {
+//         return <div>Loading...</div>;
+//     }
+    
+//     return <AllProductGridViewer productData={productData.result} />;
+// };
+
+// export default Search;
+
 "use client"
 import React, { useEffect, useState } from 'react';
-import { useSearchParams } from 'next/navigation';
-import { useRouter } from 'next/router';
+import { useSearchParams,useRouter } from 'next/navigation';
 import AllProductGridViewer from '../components/AllProductGridViewer';
 import { searchProductsFetcherFromSanity } from '@/utils/apiCalling';
 import { allProductsFetcherType } from '../../../types';
 
 const Search = () => {
-    const router = useRouter();
+    const router = useRouter(); // Using useRouter hook
     const params = useSearchParams();
     const [productData, setProductData] = useState<allProductsFetcherType | null>(null);
     
     useEffect(() => {
         if (!params.has("query")) {
-            router.push('/products');
+            router.push('/products'); // Using router.push instead of redirect
         } else {
             const searchValue = params.get("query") as string;
             const fetchData = async () => {
