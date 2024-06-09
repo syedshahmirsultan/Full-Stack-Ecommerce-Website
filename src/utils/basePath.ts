a06af7f17dea0bce3d49387679f093503aac1d83
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2024-06-09 15:05:30 +0500

    Delete src/utils/basePath.ts

--- src/utils/basePath.ts
+++
@@ -1,5 +0,0 @@
-// const BASE_PATH_FORAPI = process.env.NODE_ENV === "development" ?
-//     "http://localhost:3000"
-//     : "https://shahmir-full-stack-ecommerce-website.vercel.app/"
-
-// export default BASE_PATH_FORAPI
